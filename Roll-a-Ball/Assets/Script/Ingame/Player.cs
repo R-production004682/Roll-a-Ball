6b537d31fd6d8a457f60a3177e5143e16e17b9e0
DESKTOP-9KN8F6D\user <syk11.87kw@gmail.com>
2026-09-03 13:40:04 +0900

    プレイヤーの移動作りました

---
+++ Roll-a-Ball/Assets/Script/Ingame/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float Playerspeed;//プレイヤーの移動速度
+
+    [SerializeField]
+    private float mouseSensitivity;//マウス視点操作の感度
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))//Wキー入力
+            transform.position += Playerspeed * transform.forward * Time.deltaTime;//1秒ごとにPlayerspeedの値だけプレイヤーの正面方向に進む
+
+        if (Input.GetKey(KeyCode.S))//Sキー入力
+            transform.position -= Playerspeed * transform.forward * Time.deltaTime;//1秒ごとにPlayerspeedの値だけプレイヤーが後方に進む
+
+        if (Input.GetKey(KeyCode.D))//Dキー入力
+            transform.position += Playerspeed * transform.right * Time.deltaTime;//1秒ごとにPlayerspeedの値だけプレイヤーが右に進む
+
+        if (Input.GetKey(KeyCode.A))//Aキー入力
+            transform.position -= Playerspeed * transform.right * Time.deltaTime;//1秒ごとにPlayerspeedの値だけプレイヤーが左に進む
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;//マウスの左右移動量を取得
+        transform.Rotate(Vector3.up * mouseX);//オブジェクトのy軸を中心に回転
+
+    }
+}
