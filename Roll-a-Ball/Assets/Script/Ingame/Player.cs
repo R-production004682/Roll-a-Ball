377d157310079a8fa4e30048d11a042685004c08
DESKTOP-9KN8F6D\user <syk11.87kw@gmail.com>
2026-09-20 04:25:14 +0900

    フォーマット３

--- Roll-a-Ball/Assets/Script/Ingame/Player.cs
+++ Roll-a-Ball/Assets/Script/Ingame/Player.cs
@@ -57,8 +57,8 @@ public class Player : MonoBehaviour
         {
             if (respawnPoints.Count > 0)
             {
-                transform.position = respawnPoints[respawnPoints.Count-1];
-                transform.rotation = respawnRotations[respawnPoints.Count-1];
+                transform.position = respawnPoints[respawnPoints.Count - 1];
+                transform.rotation = respawnRotations[respawnPoints.Count - 1];
             }
         }
     }
