65da500c69e15ff3e0de4ac2cf1fec5fcdc8d291
R_production <83821881+R-production004682@users.noreply.github.com>
2026-09-21 21:02:43 +0900

    Add goal clear celebration effects

--- Roll-a-Ball/Assets/Script/Ingame/Goal.cs
+++ Roll-a-Ball/Assets/Script/Ingame/Goal.cs
@@ -1,18 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
     [SerializeField]
-    private ParticleSystem goalEffect;//ゴールエフェクト
+    private ParticleSystem goalEffect;//旧ゴールエフェクト
 
-    private void OnTriggerEnter(Collider other)//衝突判定
+    [SerializeField]
+    private GoalClearEffect clearEffect;
+
+    private bool isCompleting;
+
+    /// <summary>
+    /// プレイヤーがゴールエリアに入ったときのクリア開始処理
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))//プレイヤーとの衝突
+        if (!other.CompareTag("Player") || isCompleting)
         {
-            goalEffect.Play();//ゴールエフェクトを再生
-            GameManager.instance.StageCompleted();//クリア処理を行う
-
-            Debug.Log("ゲームクリア");
+            return;
         }
+
+        isCompleting = true;
+        if (clearEffect == null)
+        {
+            PlayLegacyGoalEffect();
+            CompleteStage();
+            return;
+        }
+
+        clearEffect.gameObject.SetActive(true);
+        clearEffect.Play();
+        StartCoroutine(CompleteStageAfterEffect());
+    }
+
+    /// <summary>
+    /// クリア演出の再生時間後にステージクリアを確定
+    /// </summary>
+    private IEnumerator CompleteStageAfterEffect()
+    {
+        yield return new WaitForSeconds(clearEffect.Duration);
+        CompleteStage();
+    }
+
+    /// <summary>
+    /// 既存のParticleSystemを使ってクリア演出を再生
+    /// </summary>
+    private void PlayLegacyGoalEffect()
+    {
+        if (goalEffect != null)
+        {
+            goalEffect.Play();
+        }
+    }
+
+    /// <summary>
+    /// ステージクリア処理を実行
+    /// </summary>
+    private void CompleteStage()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.StageCompleted();
+        }
+
+        Debug.Log("ゲームクリア");
     }
 }
